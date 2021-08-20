dbfdb070772910b43199ba40efd6a7a922f6bbec
carlosajunior <carlosajunior@hotmail.com>
2021-08-20 00:11:01 -0300

    correção na rota de listar empresas

--- start/routes.js
+++ start/routes.js
@@ -31,10 +31,9 @@ Route.post(
 Route.put("/editarEmpresa", "EmpresaController.editarEmpresa").middleware(
   "auth"
 );
-Route.get(
-  "/listarEmpresas/:empresa_user_id",
-  "EmpresaController.listarEmpresas"
-).middleware("auth");
+Route.get("/listarEmpresas", "EmpresaController.listarEmpresas").middleware(
+  "auth"
+);
 Route.delete("/excluirEmpresa", "EmpresaController.excluirEmpresa").middleware(
   "auth"
 );
